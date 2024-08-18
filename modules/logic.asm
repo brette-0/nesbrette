c9ea5cae1b1279f672e358751ec269b79665f925
Brette <98058652+brette-0@users.noreply.github.com>
2024-08-18 10:21:28 +0100

    modifed includes

---
+++ modules/logic.asm
@@ -0,0 +1,3 @@
+; FUNCTION_LOGIC
+__includeif__ INCLUDE_FUNCTION_LOGIC_RSHIFT               "../modules/logic/rshift.asm"
+__includeif__ INCLUDE_FUNCTION_LOGIC_LSHIFT               "../modules/logic/lshift.asm"
---
+++ modules/string.asm
@@ -0,0 +1,3 @@
+; FUNCTION_STRING
+__includeif__ INCLUDE_FUNCTION_STRING_MSBT                "../modules/string/msbt.asm"
+__includeif__ INCLUDE_FUNCTION_STRING_NULLT               "../modules/string/nullt.asm"
