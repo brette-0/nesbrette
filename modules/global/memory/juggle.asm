85c20e50b94716fd24b51ad72c9af68c3c72fd6a
Brette <98058652+brette-0@users.noreply.github.com>
2025-01-05 18:38:05 +0000

    realizing juggle constraints

--- modules/global/memory/juggle.asm
+++ modules/global/memory/juggle.asm
@@ -1,14 +1,12 @@
-.macro juggle __tokens__, __width__
-
+.macro juggle __tokens__, __widths__
+    ; (final element should have no width)
     ; t0 => t-1
-    memcpy  index(0, __tokens__),         index(.tcount(__tokens__),     __tokens__), __width__
+    memcpy  index(0, __tokens__),         index(.tcount(__tokens__),     __tokens__), index(0,        __widths__)
     
     .repeat .tcount(__tokens__), iter
         .if iter <> 0   ; zero is handled outside the loop
-            memcpy index(iter, __tokens__),index(iter - 1, __tokens__),  __tokens__), __width__
+            memcpy index(iter, __tokens__),index(iter - 1, __tokens__),  __tokens__), index(iter - 1, __widths__)
             .endif
         .endrepeat
     
-    ; t-1 => t-2
-    memcpy  index(.tcount(__tokens__)),   index(.tcount(__tokens__) - 1, __tokens__), __width__
     .endmacro
