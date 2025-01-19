8b8d5fad60904f7dfbb692bf6348d0a18dd5f57d
Brette <98058652+brette-0@users.noreply.github.com>
2025-01-19 08:37:46 +0000

    Typing Docs and signed math
    This is the most optimal, cleanest, tidiest highest level addition on the NES you will ever see. Also I started documenting typing

--- modules/global/math/add.asm
+++ modules/global/math/add.asm
@@ -1,7 +1,7 @@
-; untested
+; tested
 
 .macro add __output__, __mod__
-    .local skip, isoutconst, isoutnum, ismodconst, ismodnum, modtype, outtype, olabel, mlabel
+    .local skip, ahead, isoutconst, isoutnum, ismodconst, ismodnum, modtype, outtype, olabel, mlabel
     
     isoutnum    = isnum __output__
     isoutconst  = isconst __output__
@@ -12,25 +12,48 @@
     olabel      .set ilabel __output__
     mlabel      .set ilabel __mod__
 
-    modtype     .set type __mod__
-    outtype     .set type __output__
-
     detype __output__, outtype
     detype __mod__,    modtype
 
-    .out .sprintf("%d", outtype)
-    .out .sprintf("%d", modtype)
+    modwidth     = modtype & ((1 << 30) -1)
+    outwidth     = outtype & ((1 << 30) -1)
 
     ; core
-    .repeat modtype, iter
+    .repeat modwidth, iter
         lda olabel + iter
         adc mlabel + iter
         sta olabel + iter
     .endrepeat
 
-    .if outtype > modtype
+    .if outwidth > modwidth
         bcc @skip
-        inc olabel + outtype - 1
+        inc olabel + modwidth
         @skip:
     .endif
-    .endmacro
+
+    ; if types are of differing size and the type of the modifier is signed
+    ismodsign = signed modtype
+    .if ismodsign && outwidth <> modwidth
+        lda mlabel + modwidth - 1   ; load final (highest) byte of mod
+        bpl @ahead                  ; if positive, value was positive, no need to adjust
+            ; if the difference is greater than 1, we have 'borrow' logic to consider
+            .if (outwidth - modwidth) > 1
+                clc
+                .repeat outwidth - modwidth - 1, iter
+                    lda olabel + modwidth + iter
+                    sbc #$00
+                    sta olabel + modwidth + iter
+                    bcs @ahead
+                .endrepeat
+
+                ; since the last permutation would end in a bra condition, we can flesh this out optimally
+                lda olabel + outwidth - 1
+                sbc #$00
+                sta olabel + outwidth - 1
+            .else
+                ; where the difference is one, there is nowhere to borrow from
+                dec olabel + modwidth
+            .endif
+        @ahead:
+    .endif
+.endmacro
