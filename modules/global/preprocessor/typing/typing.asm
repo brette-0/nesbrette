d2c1a3b9fec884b028d62b4435c1df86c89fdd6e
Brette <98058652+brette-0@users.noreply.github.com>
2025-01-19 08:37:04 +0000

    Added typeas and fixed bugs
    Fixed signed check bug
    Fixed type bug
    Added typeas feature

--- modules/global/preprocessor/typing/typing.asm
+++ modules/global/preprocessor/typing/typing.asm
@@ -4,17 +4,33 @@
 .define itype(__token__)\
   .ident(.string(.left(1, __token__)))
 
+.define signed(__token__)\
+  __token__ & (1 << 30)
+
 .define type(__token__)\
       (.xmatch(.left(1, __token__), u8)     * 1 ) \
     | (.xmatch(.left(1, __token__), u16)    * 2 ) \
     | (.xmatch(.left(1, __token__), u24)    * 3 ) \
     | (.xmatch(.left(1, __token__), u32)    * 4 ) \
     | (.xmatch(.left(1, __token__), u64)    * 8 ) \
+    | (.xmatch(.left(1, __token__), i8)     * ((1 << 30) + 1) ) \
+    | (.xmatch(.left(1, __token__), i16)    * ((1 << 30) + 2) ) \
+    | (.xmatch(.left(1, __token__), i24)    * ((1 << 30) + 3) ) \
+    | (.xmatch(.left(1, __token__), i32)    * ((1 << 30) + 4) ) \
+    | (.xmatch(.left(1, __token__), i64)    * ((1 << 30) + 8) ) \
     | (.xmatch(.left(1, __token__), string) * -1) \
     | (.xmatch(.left(1, __token__), token)  * -2) \
     | (.xmatch(.left(1, __token__), x)      * -3) \
-    | (.xmatch(.left(1, __token__), y)      * -4)
+    | (.xmatch(.left(1, __token__), y)      * -4) \
+    | (.xmatch(.left(1, __token__), acc)    * -5)
     ;| (.xmatch(.left(1, __token__), a)      * ?) \
+
+; d31 = Reserved for special case
+; d12 = signed flag (for numbers)
+; (d0-d11) value segment
+
+.define typeas(__label__, __type__)\
+  .ident(.sprintf("w_%s", .string(__label__))) .set type __type__
 
 .define label(__token__)\
   .string(.right(1, __token__))
