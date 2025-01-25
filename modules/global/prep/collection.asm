67f8a31425433ec61071ba8384ef34d25d5b2745
Brette <98058652+brette-0@users.noreply.github.com>
2025-01-25 01:46:34 +0000

    renamed preprocessor

--- modules/global/prep/collection.asm
+++ modules/global/prep/collection.asm
@@ -1,3 +1,3 @@
 .define index(collection, _index) .right(1, {.left((_index << 1) + 1, collection)})
 .define append(collection, value) {.left(.tcount(collection), collection), value}
-.define iscollection(.define iscollection(unindicated_token) .tcount(.left(2, unindicated_token)) = 2) .tcount(.left(2, token)) = 2
+.define isarray(unindicated_token) (.tcount(.left(2, unindicated_token)) = 2)
