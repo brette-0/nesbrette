2fdb8eb62b2ec5b5ca2d8d8d457262451e995895
Brette <98058652+brette-0@users.noreply.github.com>
2025-01-28 04:59:42 +0000

    Agony

--- modules/global/memory/m,lss,ub.asm
+++ modules/global/memory/m,lss,ub.asm
@@ -1,4 +1,4 @@
-.define mssb(__constint__) \
+.define MSSB(__constint__) \
     ((__constint__ & %10000000000000000000000000000000 > %01111111111111111111111111111111) * 32) | \
     ((__constint__ & %01000000000000000000000000000000 > %10111111111111111111111111111111) * 31) | \
     ((__constint__ & %00100000000000000000000000000000 > %11011111111111111111111111111111) * 30) | \
@@ -32,7 +32,7 @@
     ((__constint__ & %00000000000000000000000000000010 > %11111111111111111111111111111101) * 2)  | \
     ((__constint__ & %00000000000000000000000000000001 > %11111111111111111111111111111110) * 1)
     
-.define lssb(__constint__) \
+.define LSSB(__constint__) \
     33 - ( \
     ((__constint__ & %00000000000000000000000000000001 > %01111111111111111111111111111111) * 32) | \
     ((__constint__ & %00000000000000000000000000000010 > %10111111111111111111111111111111) * 31) | \
@@ -67,37 +67,10 @@
     ((__constint__ & %01000000000000000000000000000000 > %11111111111111111111111111111101) * 2)  | \
     ((__constint__ & %10000000000000000000000000000000 > %11111111111111111111111111111110) * 1))
     
-.define msub(__constint__) mssb (__constint__ ^ $ffffffff)
-.define lsub(__constint__) lssb (__constint__ ^ $ffffffff)
+.define MSUB(__constint__) MSSB (__constint__ ^ $ffffffff)
+.define LSUB(__constint__) LSSB (__constint__ ^ $ffffffff)
 
-.macro MSSB __register__    ; LSSB, MSUB, LSUB (Typed)
-    .ifblank __regiter__
-        __register__ = x
-        .endif
-    
-    .if __register__ = y
-        ldy #$ff
-        @loop:
-            iny
-            lsr
-            bne @loop
-        tya
-    .elseif __register__ = x
-        ldx #$ff
-        @loop:
-            inx
-            lsr
-            bne @loop
-        txa
-    .else
-        .fatal "Invalid regsiter for MSSB"
-    .endif
-
-    adc #$00
-    .endmacro
-
-
-.macro MSSByte __target__, __indexreg__, __returnreg__, __offset__
+.macro mssbyte __target__, __regs__, __offset__
     .local targettype, targetlabel, targetsize, indexreg, returnreg
     
     targettype .set 0
@@ -109,70 +82,44 @@
     indexreg  .set 0
     returnreg .set 0
 
-    .ifblank __indexreg__
-        indexreg .set xr
+    .ifblank __regs__
+        indexreg  .set xr
+        returnreg .set ar
     .else
         setireg __indexreg__, indexreg
     .endif
 
-    .ifblank __returnreg__
-        returnreg .set ar
-    .else
-        setreg __returnreg__, returnreg
-    .endif
-
-    .ifndef __offset__
-        ldx #targetsize
+    .ifblank __offset__
+        ldr xr: imm, targetsize
     .elseif !(is_null __offset__)
-        ldx #21
+        ldr xr: imm, 21
     .endif
     
     .if targetsize > 21
-        .fatal "MSSByte cannot index array longer than 21 bytes"
+        .fatal "mssbyte cannot index array longer than 21 bytes"
     .endif
 
     .if targetsize < 1
-        .fatal "MSSByte cannot index array shorter than 1 byte"
+        .fatal "mssbyte cannot index array shorter than 1 byte"
     .endif
 
     .repeat targetsize, iter
-        lda eindex targetlabel, targetsize, iter, !(endian targettype)
+        ldr _reg: wabs, (eindex t_target: l_target, iter)
         beq @exit
-        der indexreg
+        der _ireg
     .endrepeat
 
     @exit:
-    .endmacro
-
-.macro LSSB __register__
-    .ifblank __regiter__
-        __register__ = x
-        .endif
-    
-    .if __register__ = y
-        ldy #$09
-        @loop:
-            dey
-            asl
-            bcc @loop
-        tya
-    .elseif __register__ = x
-        ldx #$09
-        @loop:
-            dey
-            asl
-            bcc @loop
-        txa
-    .else
-        .fatal "Invalid regsiter for MSSB"
+    .if _reg <> _ireg
+        trr _ireg, _reg
     .endif
-    .endmacro
+.endmacro
 
-.macro LSSByte __target__, __indexreg__, __returnreg__, __offset__
-    .local targettype, targetlabel, targetsize, indexreg, returnreg
+.macro lssbyte __target__, __indexreg__, __returnreg__, __offset__
+    .local t_target, l_target, w_target, indexreg, returnreg
 
-    targettype .set 0
-    detype __target__, targettype
+    t_target .set 0
+    detype __target__, t_target
 
     .ifnblank __offset__
         .if !(is_null __offset__)
@@ -180,40 +127,235 @@
         .endif
     .endif
 
+    .ifblank __regs__
+        indexreg = xr
+        returnreg = ar
+    .else
+        _ireg = setireg .right(1, __regs__)
+        _reg  = setreg  .left(1, __regs__ )
+    .endif
+
+    l_target  = ilabel  __target__
+    w_target  = typeval targettype
+
+    .if w_target > 21
+        .fatal "lssbyte cannot index array longer than 21 bytes"
+    .endif
+
+    .if w_target < 1
+        .fatal "lssbyte cannot index array shorter than 1 byte"
+    .endif
+
+    .repeat w_target, iter
+        ldr _reg: wabs, (eindex t_target: l_target, iter)
+        beq @exit
+        inr _ireg
+    .endrepeat
+
+    @exit:
+    .if _reg <> _ireg
+        trr _reg, _ireg
+    .endif
+.endmacro
+
+.macro msubyte __target__, __regs__, __offset__
+    .local targettype, targetlabel, targetsize, indexreg, returnreg
+    
+    targettype .set 0
+    detype __target__, targettype
+
+    targetlabel = ilabel  __target__
+    targetsize  = typeval targettype
+
     indexreg  .set 0
     returnreg .set 0
 
-    .ifblank __indexreg__
-        indexreg .set xr
+    .ifblank __regs__
+        indexreg  .set xr
+        returnreg .set ar
     .else
         setireg __indexreg__, indexreg
     .endif
 
-    .ifblank __returnreg__
-        returnreg .set ar
-    .else
-        setreg __returnreg__, returnreg
+    .ifblank __offset__
+        ldr xr: imm, targetsize
+    .elseif !(is_null __offset__)
+        ldr xr: imm, 21
     .endif
     
-    targetlabel = ilabel  __target__
-    targetsize  = typeval targettype
-
     .if targetsize > 21
-        .fatal "LSSByte cannot index array longer than 21 bytes"
+        .fatal "mssbyte cannot index array longer than 21 bytes"
     .endif
 
     .if targetsize < 1
-        .fatal "LSSByte cannot index array shorter than 1 byte"
+        .fatal "mssbyte cannot index array shorter than 1 byte"
     .endif
 
     .repeat targetsize, iter
-        lda eindex targetlabel, targetsize, iter, endian targettype
-        beq @exit
-        inr indexreg
+        ldr _reg: wabs, (eindex t_target: l_target, iter)
+        bne @exit
+        der _ireg
     .endrepeat
 
     @exit:
-    .if returnreg <> indexreg
-        trr indexreg, returnreg
+    .if _reg <> _ireg
+        trr _ireg, _reg
     .endif
+.endmacro
+
+.macro lsubyte __target__, __indexreg__, __returnreg__, __offset__
+    .local t_target, l_target, w_target, indexreg, returnreg
+
+    t_target .set 0
+    detype __target__, t_target
+
+    .ifnblank __offset__
+        .if !(is_null __offset__)
+            ldx #00
+        .endif
+    .endif
+
+    .ifblank __regs__
+        indexreg = xr
+        returnreg = ar
+    .else
+        _ireg = setireg .right(1, __regs__)
+        _reg  = setreg  .left(1, __regs__ )
+    .endif
+
+    l_target  = ilabel  __target__
+    w_target  = typeval targettype
+
+    .if w_target > 21
+        .fatal "lssbyte cannot index array longer than 21 bytes"
+    .endif
+
+    .if w_target < 1
+        .fatal "lssbyte cannot index array shorter than 1 byte"
+    .endif
+
+    .repeat w_target, iter
+        ldr _reg: wabs, (eindex t_target: l_target, iter)
+        bne @exit
+        inr _ireg
+    .endrepeat
+
+    @exit:
+    .if _reg <> _ireg
+        trr _reg, _ireg
+    .endif
+.endmacro
+
+.macro mssb __target__, __ireg__    ; LSSB, MSUB, LSUB (Typed)
+    .local _reg, _ireg
+
+    .ifblank __regs__
+        _ireg = yr   ; since 'mssbyte' uses x
+    .else
+        _ireg = setreg __ireg__
+    .endif
+
+    .if .paramcount
+        ; __mem__
+
+        .if _ireg = yr
+            mssbyte __target__, _reg: xr
+        .else
+            mssbyte __target__, _reg: yr
+        .endif
+    .endif
+
+    ldr _ireg: imm, $ff
+    @loop:
+        inr _ireg
+        lsr
+        bne @loop
+    tra _ireg
+
+    adc #$00
+.endmacro
+
+.macro lssb __target__, __ireg__    ; LSSB, MSUB, LSUB (Typed)
+    .local _reg, _ireg
+
+    .ifblank __regs__
+        _ireg = yr   ; since 'mssbyte' uses x
+    .else
+        _ireg = setreg __ireg__
+    .endif
+
+    .if .paramcount
+        ; __mem__
+
+        .if _ireg = yr
+            lssbyte __target__, _reg: xr
+        .else
+            lssbyte __target__, _reg: yr
+        .endif
+    .endif
+
+    ldr _ireg: imm, $09
+
+    @loop:
+        der _ireg
+        asl
+        bne @loop
+    tra _ireg
+.endmacro
+
+.macro msub __target__, __ireg__    ; LSSB, MSUB, LSUB (Typed)
+    .local _reg, _ireg
+
+    .ifblank __regs__
+        _ireg = yr   ; since 'mssbyte' uses x
+    .else
+        _ireg = setreg __ireg__
+    .endif
+
+    .if .paramcount
+        ; __mem__
+
+        .if _ireg = yr
+            msubyte __target__, _reg: xr
+        .else
+            msubyte __target__, _reg: yr
+        .endif
+    .endif
+
+    ldr _ireg: imm, $ff
+    @loop:
+        inr _ireg
+        lsr
+        beq @loop
+    tra _ireg
+
+    adc #$00
+.endmacro
+
+.macro lsub __target__, __ireg__    ; LSSB, MSUB, LSUB (Typed)
+    .local _reg, _ireg
+
+    .ifblank __regs__
+        _ireg = yr   ; since 'mssbyte' uses x
+    .else
+        _ireg = setreg __ireg__
+    .endif
+
+    .if .paramcount
+        ; __mem__
+
+        .if _ireg = yr
+            lsubyte __target__, _reg: xr
+        .else
+            lsubyte __target__, _reg: yr
+        .endif
+    .endif
+
+    ldr _ireg: imm, $09
+
+    @loop:
+        der _ireg
+        asl
+        beq @loop
+    tra _ireg
 .endmacro
